8ee9e604927504091a9bdf0527da06cf3dbfa865
Fernando Agustin Soza <133157005+agustinsoza@users.noreply.github.com>
2024-08-18 09:40:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ with conn.cursor() as cur:
             Velocidad_Viento VARCHAR(20),
             Clima VARCHAR(100),
             Descripcion VARCHAR(100),
-            Ultima_Actualizacion VARCHAR(100)
+            Ultima_Actualizacion VARCHAR(100),
+            PRIMARY KEY (ID, Ultima_Actualizacion)
     )
     """)
     conn.commit()
