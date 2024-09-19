633a775e775b522b4b8950ad24cace22edc5e5aa
agustinsoza <agustinsoza964@gmail.com>
2024-09-19 20:38:50 -0300

    Entrega final

--- dags/modules/__init__.py
+++ dags/modules/__init__.py
@@ -2,4 +2,3 @@ from .crear_tabla import crear_tabla
 from .cargar_tabla import cargar_tabla
 from .obtener_datos import obtener_datos
 from .enviar_email import enviar_email
-from .crear_conexion import API_KEY, REDSHIFT_SCHEMA
